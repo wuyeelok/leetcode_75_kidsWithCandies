f18cf3d3b5ecc5a8e7f4264f7d387c27a2ace82f
wuyeelok <wuyeelok@gmail.com>
2024-09-08 17:01:34 +0800

    My solution

--- src/main/java/kenneth/Solution.java
+++ src/main/java/kenneth/Solution.java
@@ -12,7 +12,47 @@ public class Solution {
     }
 
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        return new ArrayList<>();
+        // 2 <= candies.length <= 100
+        // 1 <= candies[i] <= 100
+        // 1 <= extraCandies <= 50
+
+        List<Boolean> checkResult = new ArrayList<>();
+
+        for (int i = 0; i < candies.length; i++) {
+            int maxCandiesIndex = getMaxCandiesIndex(candies, extraCandies, i);
+
+            // After looping, check if maxCandiesIndex equals to target index
+            if (i == maxCandiesIndex) {
+                checkResult.add(true);
+            } else {
+                checkResult.add(false);
+            }
+
+        }
+
+
+        return checkResult;
+    }
+
+    private static int getMaxCandiesIndex(int[] candies, int extraCandies, int i) {
+        int maxCandies = 0;
+        int maxCandiesIndex = 0;
+
+        for (int j = 0; j < candies.length; j++) {
+
+            int checkingCandies = candies[j];
+
+            if (i == j) {
+                // if checking one, add the extra candies
+                checkingCandies = checkingCandies + extraCandies;
+            }
+
+            if (checkingCandies > maxCandies) {
+                maxCandies = checkingCandies;
+                maxCandiesIndex = j;
+            }
+        }
+        return maxCandiesIndex;
     }
 
 }
